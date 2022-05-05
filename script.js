6c77961c3b6d24174e1431418f586882f1152c9d
Levan Ilashvili <levaniko900@gmail.com>
2022-05-05 21:32:48 +0400

    change constructor function to class

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 
